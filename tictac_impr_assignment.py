a99a6f830cf5d38fcc4a249dfe5bede42f275f65
Kata <katalin.molnar.kiss@gmail.com>
2017-06-28 14:12:19 +0200

    pep8 compatible

--- tictac_impr_assignment.py
+++ tictac_impr_assignment.py
@@ -1,5 +1,6 @@
 import os
 
+
 def init_board():
     board = []
     for row in range(3):
@@ -19,19 +20,20 @@ def print_board(board):
 
 def take_step(actualplayer, player, board):
 
-
     valid_step = False
     while not valid_step:
         try:
-            step = int(input("Player " + str(actualplayer + 1) + " (" + player[actualplayer] + ") take a step: "))
+            step = int(input("Player " + str(actualplayer + 1) +
+                             " (" + player[actualplayer] + ") take a step: "))
         except ValueError:
             print("Pick a number between 1 and 9!")
             continue
-        if step in range(1,10):
+        if step in range(1, 10):
             column = (step - 1) % 3
             row = int((7 - (step - column)) / 3)
-            if board[row][column] == player[0] or board[row][column] == player[1]:
-                print("Location already taken!")
+            if board[row][column] == player[0] or \
+                    board[row][column] == player[1]:
+                    print("Location already taken!")
             else:
                 os.system('clear')
                 board[row][column] = player[actualplayer]
@@ -40,28 +42,32 @@ def take_step(actualplayer, player, board):
         else:
             print("Pick a number between 1 and 9!")
 
+
 def switch_player(actualplayer):
     if actualplayer == 0:
         return 1
     else:
         return 0
 
+
 def check_won(board, player, actualplayer):
     won = False
     sign = player[actualplayer]
     for i in range(3):
-        column_won = board[0][i] == sign and board[1][i] == sign and board[2][i] == sign
-        row_won = board[i][0] == sign and board[i][1] == sign and board[i][2] == sign
-        won = won or column_won or row_won #  the won int so it keeps the new check value
-    diagonal1_won = board[0][0] == sign and board[1][1] == sign and board[2][2] == sign
-    diagonal2_won = board[2][0] == sign and board[1][1] == sign and board[0][2] == sign
+        column_won = board[0][i] == sign and \
+            board[1][i] == sign and board[2][i] == sign
+        row_won = board[i][0] == sign and \
+            board[i][1] == sign and board[i][2] == sign
+        won = won or column_won or row_won
+    diagonal1_won = board[0][0] == sign and \
+        board[1][1] == sign and board[2][2] == sign
+    diagonal2_won = board[2][0] == sign and \
+        board[1][1] == sign and board[0][2] == sign
     won = won or diagonal1_won or diagonal2_won
     if won:
-        print("Player " +str(actualplayer + 1) +  " (" + player[actualplayer] + ") won!")
+        print("Player " + str(actualplayer + 1) +
+              " (" + player[actualplayer] + ") won!")
     return won
-
-
-
 
 
 def main():
